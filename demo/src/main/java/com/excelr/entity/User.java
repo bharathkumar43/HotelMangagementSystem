4deab8b8d7759978328c901e1ab106014ada1be2
Durga-Prasad88 <durga9730cse@gmail.com>
2025-05-05 22:36:41 +0530

    Added new features to booking module

--- demo/src/main/java/com/excelr/entity/User.java
+++ demo/src/main/java/com/excelr/entity/User.java
@@ -13,8 +13,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 @Data
 public class User {
 	@Id
@@ -23,73 +21,10 @@ public class User {
 	private String name;
 	@Email(message = "Please enter valid Email")
 	private String email;
-	private Integer mobile_number;
+	private Long mobile_number;
 	private String password;
 	@Enumerated(EnumType.STRING)
 	private Role role;
 	private String image;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Integer getMobile_number() {
-		return mobile_number;
-	}
-	public void setMobile_number(Integer mobile_number) {
-		this.mobile_number = mobile_number;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Role getRole() {
-		return role;
-	}
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", mobile_number=" + mobile_number
-				+ ", password=" + password + ", role=" + role + ", image=" + image + "]";
-	}
-	public User(Integer id, String name, @Email(message = "Please enter valid Email") String email,
-			Integer mobile_number, String password, Role role, String image) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.mobile_number = mobile_number;
-		this.password = password;
-		this.role = role;
-		this.image = image;
-	}
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 	
-
 }
