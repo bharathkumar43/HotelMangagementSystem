687f0e953a0c71c266c1cbc24df22a0fafc4954f
Bharathkumar43 <bharathkumartummaganti@gmail.com>
2025-05-06 22:13:08 +0530

    second commit

--- demo/src/main/java/com/excelr/controller/UserController.java
+++ demo/src/main/java/com/excelr/controller/UserController.java
@@ -3,6 +3,7 @@ package com.excelr.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.excelr.entity.User;
 import com.excelr.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("api/user")
 
 public class UserController {
--- demo/src/main/java/com/excelr/entity/Role.java
+++ demo/src/main/java/com/excelr/entity/Role.java
@@ -2,8 +2,8 @@ package com.excelr.entity;
 
 public enum Role {
 
-	Role_ADMIN,
-	Role_USER;
+	ADMIN,
+	USER;
 	
 	public String getAuthority() {
 		return name();
